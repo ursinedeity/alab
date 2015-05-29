e9c624226c3e4259756f1a35fab20558e3c9d7db
Nan Hua <nhua@usc.edu>
2015-05-28 21:21:28 -0700

    fix bug for bedgraph with one entry

--- files.py
+++ files.py
@@ -24,29 +24,33 @@ class bedgraph(object):
     self.itr             = 0
     if not filename is None:
       if isinstance(filename,str):
-	from alab.io import loadstream
-	f = loadstream(filename)
-	readtable = np.genfromtxt(
-			    f,
-			    dtype=self.bedgraphdtype,
-			    usecols=usecols,
-			    **kwargs)
-	f.close()
+        from alab.io import loadstream
+        f = loadstream(filename)
+        readtable = np.genfromtxt(
+                            f,
+                            dtype=self.bedgraphdtype,
+                            usecols=usecols,
+                            **kwargs)
+        f.close()
       elif isinstance(filename,np.ndarray) or isinstance(filename,list):
-	readtable = np.core.records.fromarrays(
-			    np.array(filename).transpose()[[usecols]],
-			    dtype = self.bedgraphdtype)
+        readtable = np.core.records.fromarrays(
+                            np.array(filename).transpose()[[usecols]],
+                            dtype = self.bedgraphdtype)
       for line in readtable:
-	chrom = line['chrom']
-	#if np.isnan(line['value']):
-	  #line['value'] = 0
-	if not chrom in self.data:
-	  self.data[chrom] = line
-	else:
-	  self.data[chrom] = np.append(self.data[chrom],line)
+        chrom = line['chrom']
+        #if np.isnan(line['value']):
+          #line['value'] = 0
+        if not chrom in self.data:
+          self.data[chrom] = []
+          self.data[chrom].append(line)
+        else:
+          #self.data[chrom] = np.append(self.data[chrom],line)
+          self.data[chrom].append(line)
 
       for chrom in self.data:
-	self.data[chrom].sort(kind='heapsort',order='start')
+        self.data[chrom] = np.core.records.fromrecords(self.data[chrom],
+                                                      dtype = self.bedgraphdtype)
+        self.data[chrom].sort(kind='heapsort',order='start')
       
       self._flush()
       
@@ -74,9 +78,9 @@ class bedgraph(object):
       raise IndexError, "The index (%d) is out of range" % key
     for chrom in self.__sorted_keys:
       if key+1 - len(self.data[chrom]) > 0:
-	key = key - len(self.data[chrom])
+        key = key - len(self.data[chrom])
       else:
-	return self.data[chrom][key]
+        return self.data[chrom][key]
       
   #++++++++++++++++++++++++++++++++++++++++++++
   def next(self):
@@ -99,16 +103,16 @@ class bedgraph(object):
     if chrom in self.data:
       i = bisect.bisect(self.data[chrom]['end'],querystart)
       while (i < len(self.data[chrom])) and (self.data[chrom][i]['start'] < querystop):
-	intersectList.append(self.data[chrom][i])
-	i += 1
+        intersectList.append(self.data[chrom][i])
+        i += 1
     if len(intersectList) == 0:
       return None
     else:
       intersectList = np.core.records.fromrecords(intersectList,dtype=self.bedgraphdtype)
       if intersectList[0]['start'] < querystart:
-	intersectList[0]['start'] = querystart
+        intersectList[0]['start'] = querystart
       if intersectList[-1]['end'] > querystop:
-	intersectList[-1]['end'] = querystop
+        intersectList[-1]['end'] = querystop
       return intersectList
   
   #=========================================================
@@ -129,29 +133,29 @@ class bedgraph(object):
       if start > len(self) or stop > len(self) :  raise IndexError, "The index out of range"
       records = []
       for i in range(start,stop,step):
-	records.append(self.__getonerec(i))
+        records.append(self.__getonerec(i))
       return np.core.records.fromrecords(records,dtype=self.bedgraphdtype)
       
     elif isinstance(key,tuple):
       """For case a['chr1',3000000:4000000], output average value"""
       chrom = key[0]
       if not chrom in self.data:
-	raise KeyError, "Key %s doesn't exist!" % chrom
+        raise KeyError, "Key %s doesn't exist!" % chrom
       try: 
-	query = key[1]
+        query = key[1]
       except Exception:
-	raise TypeError, "Invalid argument type"
+        raise TypeError, "Invalid argument type"
       
       assert isinstance(chrom,str)
       assert isinstance(query,slice)
       if query.start == None: 
-	querystart = self.data[chrom][0]['start']
+        querystart = self.data[chrom][0]['start']
       else: 
-	querystart = query.start
+        querystart = query.start
       if query.stop == None: 
-	querystop = self.data[chrom][-1]['end']
+        querystop = self.data[chrom][-1]['end']
       else: 
-	querystop = query.stop
+        querystop = query.stop
       assert querystop > querystart
       """
       Fetch all intersection with query.start:query.stop 
@@ -159,9 +163,9 @@ class bedgraph(object):
       queryList = self.intersect(chrom,querystart,querystop)
       value = 0.0
       if not queryList is None:
-	#Sum all value
-	for rec in queryList:
-	  value += rec['value'] * (rec['end'] - rec['start'])
+        #Sum all value
+        for rec in queryList:
+          value += rec['value'] * (rec['end'] - rec['start'])
   
       return value/(querystop-querystart)
     else:
@@ -179,35 +183,31 @@ class bedgraph(object):
     assert isinstance(chrom,str)
     assert isinstance(query,slice)
     
-    new = np.array([(chrom,query.start,query.stop,value)],
-		   dtype=self.bedgraphdtype)
+    new = np.array([(chrom,query.start,query.stop,value,'')],
+                   dtype=self.bedgraphdtype)
     if not chrom in self.data:
-      self.data[chrom] = new
+      self.data[chrom] = []
+      self.data[chrom].append(new)
+      self.data[chrom] = np.core.records.fromrecords(self.data[chrom],dtype = self.bedgraphdtype)
       self._flush()
     else:
       i = bisect.bisect(self.data[chrom]['end'],query.start)
       deletelist = []
       
       if self.data[chrom][i]['start'] < query.start:
-	#print self.data[chrom][i]
-	self.data[chrom][i]['end'] = query.start
-	#print self.data[chrom][i]
-	#print "\n"
-	i += 1
+        self.data[chrom][i]['end'] = query.start
+        i += 1
       
       insertLoc = i
       while (i < len(self.data[chrom])) and (self.data[chrom][i]['end'] < query.stop):
-	#print self.data[chrom][i]
-	deletelist.append(i)
-	i += 1
+        #print self.data[chrom][i]
+        deletelist.append(i)
+        i += 1
 
       if i < len(self.data[chrom]):
-	if self.data[chrom][i]['start'] < query.stop:
-	  #print "\n"
-	  #print self.data[chrom][i]
-	  self.data[chrom][i]['start'] = query.stop
-	  #print self.data[chrom][i]
-	  
+        if self.data[chrom][i]['start'] < query.stop:
+          self.data[chrom][i]['start'] = query.stop
+          
       self.data[chrom] = np.delete(self.data[chrom],deletelist)
       self.data[chrom] = np.insert(self.data[chrom],insertLoc,new)
     #add new data finished
@@ -217,7 +217,7 @@ class bedgraph(object):
     filterList = []
     for rec in self:
       if regpattern.match(rec['flag']):
-	filterList.append(rec)
+        filterList.append(rec)
     return np.core.records.fromrecords(filterList,dtype=self.bedgraphdtype)
       
   #========================================================
@@ -229,23 +229,23 @@ class bedgraph(object):
     if bedtype == 'bedgraph':
       pattern = '%s\t%d\t%d\t'+style+'\n'
       for chrom in self.__sorted_keys:
-	for line in self.data[chrom]:
-	  f.write(pattern % (chrom,line['start'],line['end'],line['value']))
+        for line in self.data[chrom]:
+          f.write(pattern % (chrom,line['start'],line['end'],line['value']))
     elif bedtype == 'bed':
       pattern = '%s\t%d\t%d\t%s\n'
       for chrom in self.__sorted_keys:
-	for line in self.data[chrom]:
-	  f.write(pattern % (chrom,line['start'],line['end'],line['flag']))
+        for line in self.data[chrom]:
+          f.write(pattern % (chrom,line['start'],line['end'],line['flag']))
     elif bedtype == 'bedgraph+':
       pattern = '%s\t%d\t%d\t'+style+'\t%s\n'
       for chrom in self.__sorted_keys:
-	for line in self.data[chrom]:
-	  f.write(pattern % (chrom,line['start'],line['end'],line['value'],line['flag']))
+        for line in self.data[chrom]:
+          f.write(pattern % (chrom,line['start'],line['end'],line['value'],line['flag']))
     elif bedtype == 'bed+':
       pattern = '%s\t%d\t%d\t%s\t'+style+'\n'
       for chrom in self.__sorted_keys:
-	for line in self.data[chrom]:
-	  f.write(pattern % (chrom,line['start'],line['end'],line['flag'],line['value']))
+        for line in self.data[chrom]:
+          f.write(pattern % (chrom,line['start'],line['end'],line['flag'],line['value']))
     else:
       raise TypeError, "Invalid argument type %s" % (bedtype)
     f.close
