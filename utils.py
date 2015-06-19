9d2d7a0a4ae64e7df2b97b641171340e86091953
Nan Hua <nhua@usc.edu>
2015-06-19 16:35:25 -0700

    Add genome constant class

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 __author__ = 'N.H.'
+import os
+import re
+import math
+import numpy as np
+from collections import namedtuple
+from alab.io import loadstream
 
 def genchrnum(chrom):
   """ Sort by chromosome """
@@ -14,3 +20,42 @@ def genchrnum(chrom):
     num = 0
   return num
 
+class genome(object):
+  def __init__(self,genomeName,usechr=['#','X']):
+    datafile = os.environ['GENOMES'] + '/' + genomeName + '.info'
+    f = loadstream(datafile)
+    self.info = np.genfromtxt(f,dtype=[('chrom','S5'),('length',int)])
+    f.close() 
+    removepattern = ''
+    if not '#' in usechr: removepattern += '0-9'
+    if not 'X' in usechr: removepattern += 'X'
+    if not 'Y' in usechr: removepattern += 'Y'
+    if not 'M' in usechr: removepattern += 'M'
+    
+    self.info = np.delete(self.info,np.nonzero([re.search('chr['+removepattern+']',c) for c in self.info['chrom']]))
+    
+  def bininfo(self,resolution):
+    binSize    = [int(math.ceil(float(x)/resolution)) for x in self.info['length']]
+    binStart   = [sum(binSize[:i]) for i in range(len(binSize))]
+    
+    chromList  = []
+    binLabel   = []
+    for i in range(len(self.info['chrom'])):
+      chromList += [self.info['chrom'][i] for j in range(binSize[i])]
+      binLabel     += [j for j in range(binSize[i])]
+   
+    startList  = [binLabel[j]*resolution for j in range(sum(binSize))]
+    endList    = [binLabel[j]*resolution + resolution for j in range(sum(binSize))]
+    
+    binInfo    = [binSize,binStart,chromList,startList,endList]
+    return namedtuple('binInfo', 'binSize, binStart, chromList, startList, endList')._make(binInfo)
+  
+  def getchrnum(self,chrom):
+    findidx = np.flatnonzero(self.info['chrom']==chrom)
+    
+    if len(findidx) == 0:
+      return -1
+    else:
+      return findidx[0]
+  
+  
