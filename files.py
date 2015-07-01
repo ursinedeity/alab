da399429e94d394b69e796288b5a3808ec23d4c1
Nan Hua <nhua@usc.edu>
2015-07-01 11:32:25 -0700

    move genchrnum() for sorting to files.py

--- files.py
+++ files.py
@@ -5,8 +5,19 @@ __author__ = 'N.H.'
 import numpy as np
 import re
 import bisect
-from alab.utils import genchrnum
 
+def genchrnum(chrom):
+  """ Sort by chromosome """
+  if chrom:
+    num = chrom[3:]
+    if   num == 'X': num = 23
+    elif num == 'Y': num = 24
+    elif num == 'M': num = 25
+    else: num = int(num)
+  else:
+    num = 0
+  return num
+#====================================================================================
 class bedgraph(object):
   """
   Required fields
